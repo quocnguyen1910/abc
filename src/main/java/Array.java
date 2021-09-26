a6b683292075063cc248daf59087917d7741e532
quonguyen191019 <quocnguyen191019@gmail.com>
2021-09-27 01:58:28 +0700

    generate Array

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -3,62 +3,80 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("n = ");
-        int n = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("n = ");
+//        int n = sc.nextInt();
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            System.out.format("a[%d] = ", i);
-            arr[i] = sc.nextInt();
-        }
+
+//
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            System.out.format("a[%d] = ", i);
+//            arr[i] = sc.nextInt();
+//        }
+
+
+        int[] arr = generateArray(20, -50, 50);
+        printArray(arr);
+
         printArray(arr);
         System.out.format("Sum = %d\n", sumElements(arr));
         sortArrayAsc(arr);
         printArray(arr);
 
 
-//        for (int i : sotunhien) {
+//        for (int i : arr) {
 //            System.out.format("%d, ", i);
 //        }
-//        for (int i = sotunhien.length - 1; i >= 0; i--) {
-//            System.out.format("%d, ", sotunhien[i]);
+//        for (int i = arr.length - 1; i >= 0; i--) {
+//            System.out.format("%d, ", arr[i]);
 //        }
-        //putin
+
 
     }
 
+    // Ham in
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.format("%d", arr[i]);
+            System.out.format("%d, ", arr[i]);
 
         }
         System.out.println();
     }
+
     // Ham Sum
-    public static int sumElements(int[] arr){
+    public static int sumElements(int[] arr) {
         int ret = 0;
-        for (int ele : arr){
+        for (int ele : arr) {
             ret += ele;
 
         }
         return ret;
     }
-// sap xep (quick sort) - interchangeSort
-    public static void sortArrayAsc(int[] arr){{
-        for (int i = 0; i < arr.length - 1; i++){
-            for (int j = i + 1; j < arr.length; j++) {
-                if(arr[i] > arr[j]){
-                    int tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
-                }
 
+    // sap xep (quick sort) - interchangeSort
+    public static void sortArrayAsc(int[] arr) {
+        {
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i] > arr[j]) {
+                        int tmp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = tmp;
+                    }
+
+                }
             }
         }
-
-
     }
-    }
+// Ham phat sinh ramdom so trong mang
+    public static int[] generateArray(int n, int min, int max) {
+        int[] ret = new int[n];
 
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = (int)Math.floor(Math.random() * (max-min)) + min;
+
+        }
+        return  ret;
+    }
 }
